76e76c077b5d4710cd9e3fb25267ad17a2b9fc90
vinodkudkyal <vinodkudkyal2005@gmail.com>
2025-12-26 04:09:19 +0530

    New sidebar 1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Sciqus-Frontend/", // MUST match your repo name with slashes
+  base: "/", // CHANGED: Must be "/" for Render
 })
